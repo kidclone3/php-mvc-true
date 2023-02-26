3a9b2e00a4558525e4cb1a911884ace699c6a681
kidclone3 <kid.clone.3@gmail.com>
2023-02-27 02:21:14 +0700

    update db schema

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -12,20 +12,35 @@ class m0001_initial {
     public function up()
     {
         $db = Application::$app->db;
-        $SQL = "CREATE TABLE users (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                email VARCHAR(255) NOT NULL,
-                username VARCHAR(255) NOT NULL,
-                status TINYINT DEFAULT 0,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )  ENGINE=INNODB;";
+        $SQL = "create table if not exists php_mvc.users
+                (
+                    id         int auto_increment
+                        primary key,
+                    email      varchar(255)                          not null,
+                    username   varchar(255)                          not null,
+                    created_at timestamp default current_timestamp() not null,
+                    password   varchar(512)                          not null
+                );
+                create table php_mvc.userDetails
+                (
+                    firstname    varchar(255)                          not null,
+                    lastname     varchar(255)                          not null,
+                    jobTitle     varchar(255)                          not null,
+                    profileImage mediumblob                            null,
+                    dob          date      default '1900-01-01'        null,
+                    phoneNumber  varchar(20)                           not null,
+                    created_at   timestamp default current_timestamp() not null,
+                    username     varchar(255)                          not null
+                        primary key,
+                    address      varchar(255)                          null
+                );";
         $db->pdo->exec($SQL);
     }
 
     public function down()
     {
         $db = Application::$app->db;
-        $SQL = "DROP TABLE users;";
+        $SQL = "DROP TABLE php_mvc.users; DROP TABLE php_mvc.userDetails;";
         $db->pdo->exec($SQL);
     }
 }
